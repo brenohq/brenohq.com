eedf6fc9ab16c0095c221033fd94a9388571de17
Breno Henrique de Lima Freitas <brenosc2@hotmail.com>
2016-07-07 02:29:08 -0300

    Pequenas modificações.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,21 +1,21 @@
 <?php
-  if (empty($_POST['name'])  ||
-    empty($_POST['email']) 	 ||
-    empty($_POST['message']) ||
-    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
-  	echo "Preenchimento inválido!";
-  	return false;
-  }
+if (empty($_POST['name'])  ||
+empty($_POST['email']) 	 ||
+empty($_POST['message']) ||
+!filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+  echo "Preenchimento inválido!";
+  return false;
+}
 
-  $name          = $_POST['name'];
-  $email_address = $_POST['email'];
-  $message       = $_POST['message'];
+$name          = $_POST['name'];
+$email_address = $_POST['email'];
+$message       = $_POST['message'];
 
-  $to            = 'contato@brenohq.com';
-  $email_subject = "Contato do Site:  $name";
-  $email_body    = "Você recebeu uma mensagem a partir de www.brenohq.com\n\n"."Detalhes:\n\nNome: $name\nEmail: $email_address\nMensagem:\n$message";
-  $headers       = "From: contato@brenohq.com\n";
-  $headers      .= "Reply-To: $email_address";
-  mail($to,$email_subject,$email_body,$headers);
-  return true;
+$to            = 'contato@brenohq.com';
+$email_subject = "Contato do Site:  $name";
+$email_body    = "Você recebeu uma mensagem a partir de www.brenohq.com\n\n"."Detalhes:\n\nNome: $name\nEmail: $email_address\nMensagem:\n$message";
+$headers       = "From: contato@brenohq.com\n";
+$headers      .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
 ?>
