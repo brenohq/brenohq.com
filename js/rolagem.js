4d82c067da80e87e8605e13e0085c2d045a8a940
Breno Henrique <brenosc2@hotmail.com>
2016-12-08 00:46:50 -0200

    portfolio update, new css media queries and minor fixes

--- js/rolagem.js
+++ js/rolagem.js
@@ -1,10 +1,8 @@
-$(document).ready(function(){
-    $('#fullpage').fullpage({
-      	sectionsIds: ['#home', '#sobre', '#portfolio', '#contato'],
-        anchors: ['sectionHome', 'sectionSobre', 'sectionPortfolio', 'sectionContato'],
-        menu: '#navbar',
-        fitToSection: true,
-        continuousVertical: true,
-        paddingTop: '3em'
-    });
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsIds: ['#home', '#sobre', '#portfolio', '#contato'],
+    anchors: ['sectionHome', 'sectionSobre', 'sectionPortfolio', 'sectionContato'],
+    menu: '#navbar',
+    fitToSection: true
+  });
 });
