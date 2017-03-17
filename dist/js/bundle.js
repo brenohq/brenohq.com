96809f589c523aa6b93341cfc7abec02fbd2cc86
Breno Henrique <brenosc2@hotmail.com>
2017-03-17 18:22:41 -0300

    adding gulpfile and plugins

---
+++ dist/js/bundle.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+  $('#fullpage').fullpage({
+    sectionsIds: ['#home', '#sobre', '#portfolio', '#contato'],
+    anchors: ['sectionHome', 'sectionSobre', 'sectionPortfolio', 'sectionContato'],
+    menu: '#navbar',
+    fitToSection: true
+  });
+});
+'use strict';
+
+const config = {
+  strings: ['tecnologia.', 'café.', 'software livre.', 'linhas de código.', 'linux.'],
+  typeSpeed: 50,
+  backSpeed: 55,
+  startDelay: 500,
+  backDelay: 1500,
+  loop: true,
+  showCursor: true,
+  cursorChar: "|",
+
+  onFinished: function() {
+    console.log('Contribua: ' + 'https://github.com/luisvinicius167/ityped');
+  }
+}
+
+ityped.init(`#ityped`, config);
+$(function() {
+
+  $("input,textarea").jqBootstrapValidation({
+    preventSubmit: true,
+    submitError: function($form, event, errors) {
+      // additional error messages or events
+    },
+    submitSuccess: function($form, event) {
+      // Prevent spam click and default submit behaviour
+      $("#btnSubmit").attr("disabled", true);
+      event.preventDefault();
+
+      // get values from FORM
+      var name = $("input#name").val();
+      var email = $("input#email").val();
+      var message = $("textarea#message").val();
+      var firstName = name; // For Success/Failure Message
+      // Check for white space in name for Success/Fail message
+
+      if (firstName.indexOf(' ') >= 0) {
+        firstName = name.split(' ').slice(0, -1).join(' ');
+      }
+      $.ajax({
+        url: "././mail/contact_me.php",
+        type: "POST",
+        data: {
+          name: name,
+          email: email,
+          message: message
+        },
+        cache: false,
+        success: function() {
+          // Enable button & show success message
+          $("#btnSubmit").attr("disabled", false);
+          $('#success').html("<div class='alert alert-success'>");
+          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+          $('#success > .alert-success')
+          .append("<strong>Your message has been sent. </strong>");
+          $('#success > .alert-success')
+          .append('</div>');
+
+          window.confirm("Mensagem Enviada!");
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+        error: function() {
+          // Fail message
+          $('#success').html("<div class='alert alert-danger'>");
+          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+          $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+          $('#success > .alert-danger').append('</div>');
+
+          window.confirm("Erro ao enviar mensagem! Tente novamente mais tarde.");
+          //clear all fields
+          $('#contactForm').trigger("reset");
+        },
+      })
+    },
+    filter: function() {
+      return $(this).is(":visible");
+    },
+  });
+
+  $("a[data-toggle=\"tab\"]").click(function(e) {
+    e.preventDefault();
+    $(this).tab("show");
+  });
+});
+
+// When clicking on Full hide fail/success boxes
+$('#name').focus(function() {
+  $('#success').html('');
+});
---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const
+  gulp = require('gulp'),
+  concat = require('gulp-concat'),
+  imagemin = require('gulp-imagemin'),
+  rename = require('gulp-rename'),
+  uncss = require('gulp-uncss'),
+  htmlmin = require('gulp-htmlmin'),
+  size = require('gulp-size'),
+  sizereport = require('gulp-sizereport'),
+  runSequence = require('run-sequence');
+
+gulp.task('pack-js', function() {
+  return gulp.src(['src/js/rolagem.js', 'src/js/ityped.js', 'src/js/contact_me.js'])
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('pack-css', function() {
+  // Concatenando também todos os arquivos CSS
+  return gulp.src(['src/css/style.css'])
+    .pipe(concat('stylesheet.css'))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('imagemin', () =>
+  gulp.src('src/img/**/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('minify-html', function() {
+  return gulp.src(['src/index.html', 'src/404.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('remove-unused-css', function() {
+  return gulp.src('dist/css/stylesheet.css')
+    .pipe(uncss({
+      html: ['src/index.html']
+    }))
+    .pipe(gulp.dest('./dist/css/stylesheet.min.css'));
+});
+
+gulp.task('sizereport', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(sizereport());
+});
+
+var filesToMove = [
+  './src/robots.txt',
+  './src/sitemap.xml'
+];
+
+gulp.task('move', function() {
+  gulp.src(filesToMove)
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function(done) {
+  runSequence('pack-js', 'pack-css', 'imagemin', 'minify-html', 'remove-unused-css', 'move', 'sizereport', function() {
+    done();
+  });
+});
