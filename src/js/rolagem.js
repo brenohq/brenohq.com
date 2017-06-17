9e3c9a78a40b0f14b41e3885575169f91ec23270
Breno Henrique <breno.henrique@equals.com.br>
2017-06-17 10:51:26 -0300

    Adicionando Collab no portfolio

--- src/js/rolagem.js
+++ src/js/rolagem.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $('#fullpage').fullpage({
     sectionsIds: ['#home', '#sobre', '#portfolio', '#contato'],
     anchors: ['sectionHome', 'sectionSobre', 'sectionPortfolio', 'sectionContato'],
